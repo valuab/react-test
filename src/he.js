c7ebdb128ede60e6b8577eab759f017df371966c
valuab <1363907445@qq.com>
2021-06-02 10:00:44 +0800

    提交初始化版本

---
+++ src/he.js
@@ -0,0 +1,22 @@
+import React,{Component,Fragment} from 'react'
+
+class he extends Component {
+  render(){
+    return(
+      <Fragment>
+        <div>
+          <div>
+            <input /> <button>增加</button>
+            <ul>
+              <li>头部</li>
+              <li>背部</li>
+            </ul>
+          </div>
+        </div>
+      </Fragment>
+      
+    )
+  }
+}
+
+export default he
